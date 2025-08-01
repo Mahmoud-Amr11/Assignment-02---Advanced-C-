a70e4ede809d8c6436e71968e0016eddf53bdd15
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-01 05:27:19 +0300

    Q2 solve

--- Assignment 02 - Advanced C#/Program.cs
+++ Assignment 02 - Advanced C#/Program.cs
@@ -14,29 +14,68 @@
             }
             return count;
         }
+
+        static bool IsPalindrome(int[] arr)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            while (left < right)
+            {
+                if (arr[left] != arr[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
         static void Main(string[] args)
         {
             #region 1. Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
-            int n, q;
-            Console.Write("Enter the size: ");
-            n = int.Parse(Console.ReadLine());
-            Console.Write("Enter number of query: ");
-            q = int.Parse(Console.ReadLine());
+            //int n, q;
+            //Console.Write("Enter the size: ");
+            //n = int.Parse(Console.ReadLine());
+            //Console.Write("Enter number of query: ");
+            //q = int.Parse(Console.ReadLine());
 
-            int[] arr = new int[n];
+            //int[] arr = new int[n];
+            //Console.WriteLine("Enter array numbers");
+            //for (int i = 0; i < n; i++)
+            //{              
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //while (q > 0)
+            //{               
+            //    int x = int.Parse(Console.ReadLine());
+            //    int result = CountGreaterThanX(arr, x);
+            //    Console.WriteLine($"Count of numbers greater than {x}: {result}");
+            //    q--;
+            //}
+            #endregion
+
+            #region 2. Given a number N and an array of N numbers. Determine if it's palindrome or not.
+            int n = int.Parse(Console.ReadLine());
+            int[] arr= new int[n];
             Console.WriteLine("Enter array numbers");
             for (int i = 0; i < n; i++)
-            {              
+            {
                 arr[i] = int.Parse(Console.ReadLine());
             }
-            while (q > 0)
-            {               
-                int x = int.Parse(Console.ReadLine());
-                int result = CountGreaterThanX(arr, x);
-                Console.WriteLine($"Count of numbers greater than {x}: {result}");
-                q--;
+            bool isPalindrome = IsPalindrome(arr);
+
+            if (isPalindrome)
+            {
+                Console.WriteLine("YES");
             }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
             #endregion
+
+
         }
     }
 }
