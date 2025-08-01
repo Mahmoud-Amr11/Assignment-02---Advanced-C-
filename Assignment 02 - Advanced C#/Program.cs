571a90f3d238e6dc27c123d9e0e6d60d20c67f89
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-01 05:16:03 +0300

    Add project files.

---
+++ Assignment 02 - Advanced C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_02___Advanced_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
