0e1887c0657dfe1aef88ec8762d4901eb0b66ca6
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-08-01 05:32:43 +0300

    Q3 solve

--- Assignment 02 - Advanced C#/Program.cs
+++ Assignment 02 - Advanced C#/Program.cs
@@ -30,6 +30,20 @@
             }
             return true;
         }
+        static void ReverseQueue<T>(Queue<T> queue)
+        {
+            Stack<T> stack = new Stack<T>();
+
+            while (queue.Count > 0)
+            {
+                stack.Push(queue.Dequeue());
+            }
+          
+            while (stack.Count > 0)
+            {
+                queue.Enqueue(stack.Pop());
+            }
+        }
         static void Main(string[] args)
         {
             #region 1. Given an array  consists of  numbers with size N and number of queries, in each query you will be given an integer X, and you should print how many numbers in array that is greater than  X.
@@ -55,27 +69,43 @@
             #endregion
 
             #region 2. Given a number N and an array of N numbers. Determine if it's palindrome or not.
-            int n = int.Parse(Console.ReadLine());
-            int[] arr= new int[n];
-            Console.WriteLine("Enter array numbers");
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = int.Parse(Console.ReadLine());
-            }
-            bool isPalindrome = IsPalindrome(arr);
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr= new int[n];
+            //Console.WriteLine("Enter array numbers");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //bool isPalindrome = IsPalindrome(arr);
 
-            if (isPalindrome)
-            {
-                Console.WriteLine("YES");
-            }
-            else
-            {
-                Console.WriteLine("NO");
-            }
+            //if (isPalindrome)
+            //{
+            //    Console.WriteLine("YES");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO");
+            //}
 
             #endregion
 
+            #region 3. Given a Queue, implement a function to reverse the elements of a queue using a stack.
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue(5);
 
+            ReverseQueue(q);
+
+            Console.WriteLine("Reversed Queue:");
+            foreach (var item in q)
+            {
+                Console.Write(item + " ");
+            }
+
+            #endregion
         }
     }
 }
